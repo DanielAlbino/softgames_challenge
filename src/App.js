29937893b6faa6149f5eab0f689f530f2b5ed11c
Zarcade Games <127985596+ZarcadeIndieGames@users.noreply.github.com>
2024-02-13 11:42:54 +0000

    added the working task as main scene

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Stage } from "@pixi/react";
 //import { TextScene } from "./scenes/text_scene";
-//import { CardsScene } from "./scenes/cards_scene";
-import { FireParticleScene } from "./scenes/particles_scene";
+import { CardsScene } from "./scenes/cards_scene";
+//import { FireParticleScene } from "./scenes/particles_scene";
 function App() {
   return (
     <Stage options={{ backgroundColor: 0xeef1f5 }} style={{ marginLeft: 100 }}>
-     <FireParticleScene/>
+     <TextScene/>
     </Stage>
   );
 }
